87ef4fd3fc304b22d2d0146a0be4472f01f0fe05
unknown <jdavid.stevens@gmail.com>
2019-02-02 14:44:53 -0700

    mid nav

--- src/homepage/navbar_mid/nm.js
+++ src/homepage/navbar_mid/nm.js
@@ -1,12 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './nm.css';
 
 
-export default class NavbarMid extends Component{
+export default class NavbarMid extends Component {
 
-    render(){
-return(
-    <div>Navbar Mid</div>
-)
+    render() {
+        return (
+            <div className='nm'>
+                <ul className='nm-ul'>
+                    <li className='nm-li'>GALLERY</li>
+                    <li className='nm-li'>MODELS</li>
+                    <li className='nm-li'>UPCOMING EVENTS</li>
+                </ul>
+            </div>
+        )
     }
 }
