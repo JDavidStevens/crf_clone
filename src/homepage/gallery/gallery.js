3a786a04af99c33adfb7bee48f64e0f7c503ffe8
unknown <jdavid.stevens@gmail.com>
2019-02-05 15:02:07 -0700

    Styling fixes

--- src/homepage/gallery/gallery.js
+++ src/homepage/gallery/gallery.js
@@ -19,18 +19,30 @@ export default class Gallery extends Component {
                 </div>
                 <div className='gallery-photos'>
                     <div className='top-gallery'>
-                        <img src={CRF450RX} alt="CRF450RX" className='CRF450RX' />
-                        <img src={CRF250R} alt="CRF250R" className='CRF250R' />
-                        <img src={CRF450L} alt="CRF450L" className='CRF450L' />
+                        <div className='CRF450RX-container'>
+                            <img src={CRF450RX} alt="CRF450RX" className='CRF450RX' />
+                            <p className="CRF450RX-title">CRF450RX</p>
+                        </div>
+                        <div className='CRF250R-container'>
+                            <img src={CRF250R} alt="CRF250R" className='CRF250R' />
+                            <p className="CRF250R-title">CRF250R</p>
+                        </div>
+                        <div className='CRF450L-container'>
+                            <img src={CRF450L} alt="CRF450L" className='CRF450L' />
+                            <p className="CRF450L-title">CRF450L</p>
+                        </div>
                         <div className="CRF250RX-container">
                             <img src={CRF250RX} alt="CRF250RX" className="CRF250RX" />
+                            <p className="CRF250RX-title">CRF250RX</p>
                         </div>
                     </div>
-                    <div>
-                    </div>
-                    <div>
+                    <div className='CRF450X-container'>
                         <img src={CRF450X} alt="CRF450X" className='CRF450X' />
+                        <p className="CRF450X-title">CRF450X</p>
+                    </div>
+                    <div className='CRF450RWE-container'>
                         <img src={CRF450RWE} alt="CRF450RWE" className='CRF450RWE' />
+                        <p className="CRF450RWE-title">CRF450RWE</p>
                     </div>
                 </div>
             </div>
