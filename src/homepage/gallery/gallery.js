efebc0369ee1653b7eb02bf40e43ef7ac6da4810
unknown <jdavid.stevens@gmail.com>
2019-02-04 17:10:45 -0700

    Gallery images aligned

--- src/homepage/gallery/gallery.js
+++ src/homepage/gallery/gallery.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './gallery.css';
 import CRF450RX from '../../assets/CRF Performance Line - Honda Powersports_files/Collective_CRF450RX_01.jpg';
 import CRF250R from '../../assets/CRF Performance Line - Honda Powersports_files/Collective_CRF250RX_04.jpg';
@@ -8,24 +8,32 @@ import CRF450X from '../../assets/CRF Performance Line - Honda Powersports_files
 import CRF450RWE from '../../assets/CRF Performance Line - Honda Powersports_files/Collective_CRF450RWE_06.jpg';
 
 
-export default class Gallery extends Component{
+export default class Gallery extends Component {
 
-    render(){
-return(
-    <div className='gallery'>
-    <div>
-        <h3 className='gallery-h3'>The Honda CRF Performance Line</h3>
-        <h2 className='gallery-h2'>GALLERY</h2>
-    </div>
-    <div>
-       <img src={CRF450RX} alt="CRF450RX"/>
-       <img src={CRF250R} alt="CRF250R"/>
-       <img src={CRF450L} alt="CRF450L"/>
-       <img src={CRF250RX} alt="CRF250RX"/>
-       <img src={CRF450X} alt="CRF450X"/>
-       <img src={CRF450RWE} alt="CRF450RWE"/>
-    </div>
-    </div>
-)
+    render() {
+        return (
+            <div className='gallery' >
+                <div>
+                    <h3 className='gallery-h3'>The Honda CRF Performance Line</h3>
+                    <h2 className='gallery-h2'>GALLERY</h2>
+                </div>
+                <div className='gallery-photos'>
+                    <div className='top-gallery'>
+                        <img src={CRF450RX} alt="CRF450RX" className='CRF450RX' />
+                        <img src={CRF250R} alt="CRF250R" className='CRF250R' />
+                        <img src={CRF450L} alt="CRF450L" className='CRF450L' />
+                        <div className="CRF250RX-container">
+                            <img src={CRF250RX} alt="CRF250RX" className="CRF250RX" />
+                        </div>
+                    </div>
+                    <div>
+                    </div>
+                    <div>
+                        <img src={CRF450X} alt="CRF450X" className='CRF450X' />
+                        <img src={CRF450RWE} alt="CRF450RWE" className='CRF450RWE' />
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
