2c62df84ac2017addb0ce2cc7beb4a6d3faa0c79
unknown <jdavid.stevens@gmail.com>
2019-02-11 14:11:24 -0700

    Events and connected

--- src/homepage/connected/connected.js
+++ src/homepage/connected/connected.js
@@ -1,12 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './connected.css';
 
 
-export default class Connected extends Component{
+export default class Connected extends Component {
 
-    render(){
-return(
-    <div>Connected</div>
-)
+    constructor() {
+        super()
+        this.state = {
+            first: '',
+            last: '',
+            email: '',
+            zip: ''
+        }
+    }
+
+    render() {
+        return (
+            <div className='connected'>
+                <div className='connected-title'>
+                    <h2 className='connected-h2'>STAY CONNECTED</h2>
+                    <p className='connected-p'>Stay up-to-date on the latest news from the CRF Performance Line</p>
+                </div>
+                <div className='connected-inputs-container'>
+                    <input className='connected-inputs' placeholder='FIRST NAME' onChange={e => this.setState({ first: e.target.value })}></input>
+                    <input className='connected-inputs' placeholder='LAST NAME' onChange={e => this.setState({ last: e.target.value })}></input>
+                    <input className='connected-inputs' placeholder='EMAIL' onChange={e => this.setState({ email: e.target.value })}></input>
+                    <input className='connected-inputs' placeholder='ZIP CODE' onChange={e => this.setState({ zip: e.target.value })}></input>
+                </div>
+                <div className='connected-opt-submit-container'>
+                    <div className='connected-opt-in'></div>
+                    <div className='connected-submit'>
+                        <button>SUBMIT</button>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
--- src/homepage/events/events.js
+++ src/homepage/events/events.js
@@ -6,7 +6,14 @@ export default class Events extends Component{
 
     render(){
 return(
-    <div>Events</div>
+    <div className='events'>
+    <div className='events-bg'>
+    <div className='events-overlay'>
+    <h2 className='events-h2'>RACES AND EVENTS</h2>
+    <button className='events-button'>VIEW UPCOMING EVENTS</button>
+    </div>
+    </div>
+    </div>
 )
     }
 }
